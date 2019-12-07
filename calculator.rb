61081bc445888a88cbc718843129dc2669c84303
Khalid Tissire <ghettoguy7@gmail.com>
2019-12-07 21:46:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,5 @@ first_number = 0
 second_number = 0
 sum = first_number + second_number
 difference = first_number - second_number
-  = first_number * second_number
+product = first_number * second_number
    = first_number + second_number
